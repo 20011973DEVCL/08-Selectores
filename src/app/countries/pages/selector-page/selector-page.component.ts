689603d962e12ce471c01b16ca0f0993d3fceb06
Danilo Alarcon Lopez <danilo.alarcon.lopez@icloud.com>
2023-11-22 16:45:03 -0300

    se completa seccion seelectores

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { filter, switchMap, tap } from 'rxjs';
+
+import { Region, SmallCountry } from '../../interfaces/country.interfaces';
+import { CountriesService } from '../../services/countries.service';
+
 
 @Component({
   selector: 'app-selector-page',
@@ -7,7 +12,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styles: [
   ]
 })
-export class SelectorPageComponent  {
+export class SelectorPageComponent implements OnInit  {
+
+  public countriesByRegion: SmallCountry[]=[];
+  public borders: SmallCountry[]=[];
 
   public myForm: FormGroup = this.fb.group({
     region : ['', Validators.required ],
@@ -18,7 +26,70 @@ export class SelectorPageComponent  {
 
   constructor(
     private fb: FormBuilder,
+    private countriesService: CountriesService
   ) {}
+
+  ngOnInit(): void {
+   this.onRegionChanged();
+   this.onCountryChanged();
+  }
+
+  get regions():Region[]
+  {
+    return this.countriesService.regions;
+  }
+
+  onRegionChanged()
+  {
+    this.myForm.get('region')?.valueChanges
+      .pipe(
+        tap( ()=> this.myForm.get('country')?.setValue('')),
+        tap( ()=> this.borders = []),
+        switchMap(region => this.countriesService.getCountriesByRegion(region)),
+      )
+      .subscribe(countries => {
+        this.countriesByRegion = countries.sort((a, b) => {
+          const nameA = a.name.toUpperCase();
+          const nameB = b.name.toUpperCase();
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        });
+      });
+  }
+
+  onCountryChanged()
+  {
+    this.myForm.get('country')?.valueChanges
+    .pipe(
+      tap( ()=> this.myForm.get('border')?.setValue('')),
+      filter( (value: string) => value.length > 0),
+      switchMap(alphacode => this.countriesService.getCountryByAlphaCode(alphacode)),
+      switchMap((country) => this.countriesService.getCountryBordersByCodes(country.borders))
+    )
+    .subscribe(country => {
+      // this.countriesByRegion = borders.sort((a, b) => {
+      //   const nameA = a.name.toUpperCase();
+      //   const nameB = b.name.toUpperCase();
+      //   if (nameA < nameB) {
+      //     return -1;
+      //   }
+      //   if (nameA > nameB) {
+      //     return 1;
+      //   }
+      //   return 0;
+      // });
+      this.borders = country;
+      console.log({borders: country});
+    });
+  }
+
+
+
 
 
 
